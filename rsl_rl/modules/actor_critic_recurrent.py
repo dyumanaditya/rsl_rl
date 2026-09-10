c8f3d130f8ecff26c67399900a8b2d32ed67648c
Dyuman Aditya <dyuman2001@gmail.com>
2026-09-10 00:13:43 -0700

    Roll back PPO

--- rsl_rl/modules/actor_critic_recurrent.py
+++ rsl_rl/modules/actor_critic_recurrent.py
@@ -27,7 +27,6 @@ class ActorCriticRecurrent(ActorCritic):
         rnn_hidden_size=256,
         rnn_num_layers=1,
         init_noise_std=1.0,
-        actor_output_init_scale=None,
         **kwargs,
     ):
         if kwargs:
@@ -43,7 +42,6 @@ class ActorCriticRecurrent(ActorCritic):
             critic_hidden_dims=critic_hidden_dims,
             activation=activation,
             init_noise_std=init_noise_std,
-            actor_output_init_scale=actor_output_init_scale,
         )
 
         activation = resolve_nn_activation(activation)
